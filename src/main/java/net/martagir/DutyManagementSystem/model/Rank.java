570c0076bda9169ad1552414498ae9d1b30d5e91
martagir <martagir@yandex.ru>
2018-05-29 11:06:17 +0300

    29.05.2018 начало новой переработки

--- src/main/java/net/martagir/DutyManagementSystem/model/Rank.java
+++ src/main/java/net/martagir/DutyManagementSystem/model/Rank.java
@@ -5,19 +5,17 @@ import javax.persistence.*;
 /**
  * Created by Marat <martagir@yandex.ru> on 29.03.2018.
  */
+@Entity
+@Table(name = "RANKS")
 public class Rank {
-    @Entity
-    @Table(name = "RANKS")
-    public class Soldier {
-        @Id
-        @Column(name = "ID")
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private int id;
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-        @Column(name = "NAME")
-        private String name;
+    @Column(name = "NAME")
+    private String name;
 
-        @Column(name = "SHORTNAME")
-        private String shortName;
-    }
+    @Column(name = "SHORTNAME")
+    private String shortName;
 }
